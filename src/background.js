20a1a2a453e70d448ee464f0d601d8af26ed4394
Igibek Koishybayev <koishybayev.igibek@gmail.com>
2017-10-28 23:23:43 -0400

    update extension

--- src/background.js
+++ src/background.js
@@ -1,3 +1,9 @@
+// Called when the user installs the extension.
+chrome.runtime.onInstalled.addListener(function (object) {
+  chrome.tabs.create({url: "https://igibek.github.io/latynga"}, function (tab) {
+      console.log("New tab launched with https://igibek.github.io/latynga");
+  });
+});
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
     // Send a message to the active tab
@@ -5,4 +11,4 @@ chrome.browserAction.onClicked.addListener(function(tab) {
       var activeTab = tabs[0];
       chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
     });
-  });
+});
