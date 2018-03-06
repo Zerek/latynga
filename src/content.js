2268b2bdcdf89488e5f32af3cd82fbe21f191285
Igibek Koishybayev <koishybayev.igibek@gmail.com>
2018-03-06 18:24:20 -0500

    updated the src

--- src/content.js
+++ src/content.js
@@ -1,11 +1,19 @@
 //<div>Icons made by <a href="https://www.flaticon.com/authors/twitter" title="Twitter">Twitter</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-var old = ["а", "ә", "б", "д", "е", "ф", "г", "ғ", "х", "Һ", "і", "и", "й", "ж", "к", "л", "м", "н", "ң", "нг", "о", "ө", "п", "қ", "р", "с", "ш", "ч", "т", "ұ", "ү", "в", "ы", "у", "з"];
-var nea = ["a", "a'", "b", "d", "e", "f", "g", "g'", "h", "Һ", "i", "i'", "i'", "j", "k", "l", "m", "n", "n'", "n'", "o", "o'", "p", "q", "r", "s", "s'", "c'", "t", "u", "u'", "v", "y", "y'", "z"];
+var old = ["а", "ә", "б", "д", "е", "ф", "г", "ғ", "х", "Һ", "і", "и", "й", "ж", "к", "л", "м", "н", "ң", "о", "ө", "п", "қ", "р", "с", "ш", "ч", "т", "ұ", "ү", "в", "ы", "у", "з"];
+var nea = ["a", "á", "b", "d", "e", "f", "g", "ǵ", "h", "Һ", "i", "ı", "ı", "j", "k", "l", "m", "n", "ń", "o", "ó", "p", "q", "r", "s", "sh", "ch", "t", "u", "ú", "v", "y", "ý", "z"];
+var previousBody = null;
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if( request.message === "clicked_browser_action" ) {
+        if(previousBody !== null) {
+          $("body").html(previousBody);
+          previousBody = null;
+          return
+        }
+
         let bodyHtml = $("body").html();
+        previousBody = bodyHtml;
         
         var i = 0;
         console.log("started replacing");
@@ -15,10 +23,10 @@ chrome.runtime.onMessage.addListener(
         }
         bodyHtml = bodyHtml.replace(/ц/g,"s");
         bodyHtml = bodyHtml.replace(/Ц/g,"S");
-        bodyHtml = bodyHtml.replace(/щ/g,"c'");
-        bodyHtml = bodyHtml.replace(/Щ/g,"C'");
-        bodyHtml = bodyHtml.replace(/я/g,"i'a");
-        bodyHtml = bodyHtml.replace(/Я/g,"I'a");
+        bodyHtml = bodyHtml.replace(/щ/g,"ch");
+        bodyHtml = bodyHtml.replace(/Щ/g,"Ch");
+        bodyHtml = bodyHtml.replace(/я/g,"ıa");
+        bodyHtml = bodyHtml.replace(/Я/g,"Ia");
         bodyHtml = bodyHtml.replace(/э/g,"e");
         bodyHtml = bodyHtml.replace(/Э/g,"E");
         bodyHtml = bodyHtml.replace(/ь/g,"");
