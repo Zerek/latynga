86dc940bed307d55445f961acb217750d2be6242
igibek <koishybayev.igibek@gmail.com>
2021-02-19 10:24:13 -0500

    removed unused code

--- src/js/background.js
+++ src/js/background.js
@@ -4,11 +4,4 @@ chrome.runtime.onInstalled.addListener(function (object) {
       console.log("Open new tab https://zerek.kz");
   });
 });
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-    // Send a message to the active tab
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      var activeTab = tabs[0];
-      chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-    });
-});
+
